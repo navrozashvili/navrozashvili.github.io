f724167f7d18b8ce645c85907dd01aa49f68b4c2
navrozashvili <71011169+navrozashvili@users.noreply.github.com>
2021-03-25 03:23:10 +0400

    added ability to pick and set static color
    its 3AM and if I come up with another idea today ill delete repo

--- avea.js
+++ avea.js
@@ -47,6 +47,16 @@ async function startColorSequence() {
   }
 
 }
+function setStaticColor()
+{
+  var color = hexToRgb(document.getElementById("staticColor").value);
+  setRGBnoW(color[0],color[1],color[2]);
+}
+const hexToRgb = hex =>
+  hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
+             ,(m, r, g, b) => '#' + r + r + g + g + b + b)
+    .substring(1).match(/.{2}/g)
+    .map(x => parseInt(x, 16))
 function setBrightnessFromSlider() {
   var percentage = document.getElementById("brightnesSlider").value;
   setBrightnessByPercentage(percentage);
